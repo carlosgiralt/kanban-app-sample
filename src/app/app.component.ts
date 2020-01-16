2d12055272c662d8533701da7f4388ea75da7c0a
Carlos Alberto Giralt Torriente <carlosgt87@gmail.com>
2020-01-16 11:02:30 -0500

    display dummy data of kanban board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-app-example';
+
+  workflows = ['ToDo', 'Doing', 'Done'];
+
+  board = {
+    ToDo: [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ],
+    Doing: [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ],
+    Done: []
+  };
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
